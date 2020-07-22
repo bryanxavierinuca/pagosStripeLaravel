b95e571e4360bc8b83ef1cf8dec003cd709e9543
bryanxavierinuca <bryanxavier95@gmail.com>
2020-07-22 15:34:00 -0500

    Probando laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Xavier Inuca
                 </div>
 
                 <div class="links">
